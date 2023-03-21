5ae436a80df9ac43d43369003fa6b6612cea9156
Oleg Istomin <Oleg.Istomin@infobip.com>
2023-03-21 15:47:40 +0300

    upd

--- apps/web/src/app/order/order.component.ts
+++ apps/web/src/app/order/order.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
+  webApp: any
+
   constructor() { }
 
   ngOnInit(): void {
+    // @ts-ignore
+    this.webApp = JSON.stringify(window.Telegram.WebApp, null, 2)
   }
 
 }
